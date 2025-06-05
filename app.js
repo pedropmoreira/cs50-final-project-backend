a891e47a34a1f444d386365269f98e231d696efb
Pedro Paulo <pedropscontatoprof@gmail.com>
2025-06-05 10:40:27 -0300

    Installing CORS , fast fix I forgot to create the fix branch

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ import express from 'express';
 import categoryRouter from './routes/categoryRouter.js'
 import transactionRouter from './routes/transactionRouter.js'
 import userRouter from './routes/userRouter.js'
+import cors from "cors"; 
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3002", 
+    credentials: true, 
+}));
+
 const PORT = 3000;
 
 app.use(express.json());
